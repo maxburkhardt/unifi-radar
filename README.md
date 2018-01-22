0750dea966af60278e3f03830d15fd5f81f26d1f
Max Burkhardt <max.burkhardt@airbnb.com>
2018-01-22 01:04:27 -0800

    Emit events to SQS when devices are detected

--- README.md
+++ README.md
@@ -6,17 +6,19 @@ Amazon AWS SQS.
 
 ## Installation
 
-Clone the repo. Run `lein uberjar`
+Clone the repo. Run `lein uberjar`.
+
+The `recognized-devices` config option should be a map of device name to a
+color to indicate on the Aurora, such as:
+`{"Device Name" {:hue 0, :saturation 100, :brightness 100}}`
 
 ## Usage
-
-FIXME: explanation
 
     $ java -jar radar-0.1.0.jar [args]
 
 ## License
 
-Copyright © 2018 FIXME
+Copyright © 2018 Max Burkhardt
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/radar/core.clj
+++ src/radar/core.clj
@@ -1,8 +1,18 @@
 (ns radar.core
   (:gen-class)
   (:require [clj-time.local :as localtime]
-            [clj-time.coerce :as coercetime])
+            [clj-time.coerce :as coercetime]
+            [cheshire.core :refer :all]
+            [outpace.config :refer [defconfig]]
+            [cemerick.bandalore :as sqs])
   (:use [radar.unifi]))
+
+;; Configuration for the devices to recognize
+(defconfig ^:required recognized-devices)
+(defconfig ^:required sqs-queue-url)
+(defconfig ^:required aws-client-id)
+(defconfig ^:required aws-client-secret)
+(def grey {:hue 0 :saturation 0 :brightness 50})
 
 (defn get-wifi-client-names
   "Get the human-readable names for connected devices"
@@ -15,10 +25,18 @@
 (defn -main
   "Monitor wifi clients attached to UniFi and emit Aurora events"
   [& args]
-  (loop [seen (get-wifi-client-names)]
-    (let [latest (get-wifi-client-names)]
-      ;; Get the hosts that are only in the latest sweep
-      (println (clojure.set/difference latest seen))
-      ;; Avoid crushing the UniFi server
-      (Thread/sleep 5000)
-      (recur latest))))
+  (let [q sqs-queue-url client (sqs/create-client aws-client-id aws-client-secret)]
+    (loop [seen (get-wifi-client-names)]
+      (let [latest (get-wifi-client-names)]
+        ;; Get the hosts that are only in the latest sweep
+        (let [diff (clojure.set/difference latest seen)]
+          (if (> (count diff) 0)
+            (sqs/send
+              client
+              q
+              (generate-string
+                {:type :flow
+                 :palette [grey (get recognized-devices (first diff)) grey]}))))
+        ;; Avoid crushing the UniFi server
+        (Thread/sleep 5000)
+        (recur latest)))))
