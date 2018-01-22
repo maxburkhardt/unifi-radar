074a487890a7b1dca69e5216f79dab6112d15021
Max Burkhardt <max.burkhardt@airbnb.com>
2018-01-22 00:38:12 -0800

    Use sets for more logical diffing

--- src/radar/core.clj
+++ src/radar/core.clj
@@ -1,26 +1,24 @@
 (ns radar.core
   (:gen-class)
-  (:require [clojure.data :refer [diff]]
-            [clj-time.core :as coretime]
-            [clj-time.local :as localtime]
+  (:require [clj-time.local :as localtime]
             [clj-time.coerce :as coercetime])
   (:use [radar.unifi]))
 
 (defn get-wifi-client-names
   "Get the human-readable names for connected devices"
   []
-  (map (fn [client] (get client "hostname"))
+  (set (map (fn [client] (get client "hostname"))
        (filter (fn [client] (>= (get client "last_seen")
                                 (- (int (/ (coercetime/to-long (localtime/local-now)) 1000)) 300)))
-                 (clients))))
+                 (clients)))))
 
 (defn -main
   "Monitor wifi clients attached to UniFi and emit Aurora events"
   [& args]
   (loop [seen (get-wifi-client-names)]
     (let [latest (get-wifi-client-names)]
-      (do
-        (Thread/sleep 5000)
-        ;; Get the hosts that are only in the latest sweep
-        (println (get (diff seen latest) 1))
-        (recur latest)))))
+      ;; Get the hosts that are only in the latest sweep
+      (println (clojure.set/difference latest seen))
+      ;; Avoid crushing the UniFi server
+      (Thread/sleep 5000)
+      (recur latest))))
